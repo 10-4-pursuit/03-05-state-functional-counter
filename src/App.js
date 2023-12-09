1eb6dd34a884c453bf8c198dcbc5f3d63ee7d0f4
athomas227 <andrewthomas@pursuit.org>
2023-12-09 15:51:27 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;

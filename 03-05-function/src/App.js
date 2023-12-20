414cc78c24a1be09b19fca7097fbab14931fc3fb
bperalta14 <briannaperalta@pursuit.org>
2023-12-20 13:57:31 -0500

    Add Counter component

---
+++ 03-05-function/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './Counter';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, this is my counter!</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
